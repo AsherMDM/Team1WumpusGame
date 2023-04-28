20ff78372575fafb27feb212dce1be497feabaa1
AsherMDM <2026324@apps.nsd.org>
2023-04-28 13:24:37 -0700

    all class

--- Main Project/WumpusGame/Player.cs
+++ Main Project/WumpusGame/Player.cs
@@ -13,6 +13,34 @@ namespace WumpusGame
 
         }
 
+        int amountOfArrows = 3;
+        int amountOfCoins = 10;
+        int turnAmount = 0;
 
-    }
+        public int[] GetInventory()
+        {
+            // Returns an array of the amounts of all objects in players inv
+            // arrowsCount, goldCoinsCount
+            return { 1, 2 }
+        }
+
+        public void UpdateArrowAmount(int update)
+        {
+            // Updates Arrow Amount
+        }
+
+        public void UpdateCoinAmount(int update)
+        {
+            // Updates Coin Amount
+        }
+
+        public void ShootArrow(int cave, )
+        {
+            // SHoots arrow
+        }
+
+        public int CalculateScore()
+        {
+            //Calculates Final Score
+        }
 }
