a4be0ffc51cabeb305d38522682d26c808724fc8
poyatoya <2008745@apps.nsd.org>
2023-04-28 21:40:46 -0700

    update cave.cs

--- Main Project/WumpusGame/Cave.cs
+++ Main Project/WumpusGame/Cave.cs
@@ -16,13 +16,6 @@ namespace WumpusTest
             // read the file that stores the room connections
             // store it into the Array of list
         } 
-        public void ReadFromFile()
-        {
-            StreamReader sr = new StreamReader("MapofCave.txt");
-            string caves = sr.ReadLine();
-            sdfdghjklk
-            
-        }
 
         public List<int> get_possible_moves(int currentRoom)
         {
