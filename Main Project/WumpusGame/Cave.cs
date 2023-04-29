4ae20b21ff74d43d45175802ae28f3f88b0e4264
poyatoya <2008745@apps.nsd.org>
2023-04-28 20:05:21 -0700

    copy cave.cs to main form

---
+++ Main Project/WumpusGame/Cave.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WumpusTest
+{
+    // Array of list to store room connections
+    private List<int>[] adjacent = new List<int>[30];
+
+    public class Cave
+    {
+        public Cave()
+        {
+            // read the file that stores the room connections
+            // store it into the Array of list
+        } 
+
+        public List<int> get_possible_moves(int currentRoom)
+        {
+            // find adjacent rooms for currentRoom
+            // return list of adjacent rooms
+            
+        }
+
+    }
+}
