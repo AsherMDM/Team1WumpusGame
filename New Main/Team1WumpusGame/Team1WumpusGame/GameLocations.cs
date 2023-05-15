2f72530eb8590e4ed15478109d8ca3e9613d5689
AsherMDM <2026324@apps.nsd.org>
2023-05-15 12:39:35 -0700

    added classes

---
+++ New Main/Team1WumpusGame/Team1WumpusGame/GameLocations.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team1WumpusGame
+{
+    internal class GameLocations
+    {
+        public GameLocations()
+        {
+
+        }
+
+        Random rnd = new Random();
+
+        int wumpusLocation = 1;
+        int[] batLocations = { 0, 0, 0 };
+        int[] pitLocations = { 0, 0, 0 };
+
+        public void GenerateWumpusLocation()
+        {
+            int i = rnd.Next(1, 30);
+            wumpusLocation = i;
+        }
+
+        public int GetWumpusLocation()
+        {
+            return wumpusLocation;
+        }
+
+        public void GenerateBatLocations()
+        {
+            batLocations[0] = rnd.Next(1, 30);
+            batLocations[1] = rnd.Next(1, 30);
+            batLocations[2] = rnd.Next(1, 30);
+        }
+        public int[] GetBatLocations()
+        {
+            return batLocations;
+        }
+
+        public void GeneratePitLocations()
+        {
+            pitLocations[0] = rnd.Next(1, 30);
+            pitLocations[1] = rnd.Next(1, 30);
+            pitLocations[2] = rnd.Next(1, 30);
+        }
+        public int[] GetPitLocations()
+        {
+            return pitLocations;
+        }
+
+        public int MoveWumpus()
+        {
+            return wumpusLocation;
+        }
+
+        public bool[] findAdjacentHazards(int[] adjacentCaves, int[] batLocs, int[] pitLocs, int wumpusLoc)
+        {
+            bool[] warnings = new bool[3] { false, false, false };
+
+
+            foreach (int cave in adjacentCaves)
+            {
+                foreach (int caveBat in batLocs)
+                {
+                    if (caveBat == cave)
+                    {
+                        warnings[0] = true;
+                    }
+                }
+
+                foreach (int cavePit in pitLocs)
+                {
+                    if (cavePit == cave)
+                    {
+                        warnings[1] = true;
+                    }
+                }
+
+                if (cave == wumpusLoc)
+                {
+                    warnings[2] = true;
+                }
+            }
+
+            return warnings;
+        }
+
+        public bool[] findCurrentHazards(int cave, int[] batLocs, int[] pitLocs, int wumpusLoc)
+        {
+            bool[] warnings = new bool[3] { false, false, false };
+
+            foreach (int caveBat in batLocs)
+            {
+                if (caveBat == cave)
+                {
+                    warnings[0] = true;
+                }
+            }
+
+            foreach (int cavePit in pitLocs)
+            {
+                if (cavePit == cave)
+                {
+                    warnings[1] = true;
+                }
+            }
+
+            if (cave == wumpusLoc)
+            {
+                warnings[2] = true;
+            }
+
+            return warnings;
+        }
+    }
+}
---
+++ New Main/Team1WumpusGame/Team1WumpusGame/Player.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team1WumpusGame
+{
+    internal class Player
+    {
+        public Player()
+        {
+
+        }
+
+        //   arrows coins
+        int[] inventory = { 3, 10 };
+        int turnAmount = 0;
+        int playerLocation = 1;
+
+        public int[] GetInventory()
+        {
+            return inventory;
+        }
+
+        public void UpdateArrowAmount(int update)
+        {
+            inventory[0] += update;
+        }
+
+        public void UpdateCoinAmount(int update)
+        {
+            inventory[1] += update;
+        }
+
+        public bool ShootArrow(int cave, int wumpusLoc)
+        {
+            inventory[0]--;
+            if (cave == wumpusLoc)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int GetPlayerLocation()
+        {
+            return playerLocation;
+        }
+
+        public void MovePlayer(int newLocation)
+        {
+            playerLocation = newLocation;
+            turnAmount++;
+        }
+        public int CalculateScore(bool win)
+        {
+            // 100 - number of turns + gold coins + (5 * Arrows) + 50 ( if won )
+            int score = 100 - turnAmount + inventory[1] + (5 * inventory[0]);
+            if (win)
+            {
+                score += 50;
+            }
+            return score;
+        }
+    }
+}
