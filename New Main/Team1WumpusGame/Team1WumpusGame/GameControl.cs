41fa9ca2755a355b7765d6d503116ae3fa62db13
RayanRizwan <63744820+RayanRizwan@users.noreply.github.com>
2023-05-15 13:13:07 -0700

    gfdg

---
+++ New Main/Team1WumpusGame/Team1WumpusGame/GameControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team1WumpusGame
+{
+    public class GameControl
+    {
+        public Question GetQuestion()
+        {
+            fdf
+        }
+    }
+}
--- New Main/Team1WumpusGame/Team1WumpusGame/Question.cs
+++ New Main/Team1WumpusGame/Team1WumpusGame/Question.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Team1WumpusGame
 {
-    internal class Question
+    public class Question
     {
+        public string Ques { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public string CorrectAnswer { get; set; }
+
+        public Question(string ques, string answer1, string answer2, string answer3, string answer4, string correctAnswer)
+        {
+            Ques = ques;
+            Answer1 = answer1;
+            Answer2 = answer2;
+            Answer3 = answer3;
+            Answer4 = answer4;
+            CorrectAnswer = correctAnswer;
+        }
     }
 }
